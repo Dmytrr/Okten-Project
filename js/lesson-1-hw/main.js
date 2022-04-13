6d51de7bcd50e3fd66009055638f5bb4e5308cf7
Dmytrr <dimonpavlo@yahoo.com>
2022-04-14 02:02:30 +0300

    hw-cards-3 added

---
+++ js/lesson-1-hw/main.js
@@ -0,0 +1,49 @@
+let greeting = 'hello';
+let site = 'owu';
+let domain = 'com';
+let country = 'ua';
+let num1 = 1;
+let num2 = 10;
+let num3 = -999;
+let num4 = 123;
+let pi = 3.14;
+let num5 = 2.7;
+let num6 = 16;
+let dog = true;
+let cat = false;
+
+
+let book = {name: 'Easy Java', pages: 77, genre: 'fantasy'};
+
+
+let book2 = {name: 'Die Hard Java', pages: 9999, genre: 'horror', authors: ['Stephen King', 'Howard Lovecraft', 'Alfred Hitchcock']};
+
+
+let firstName = 'Dmytro';
+let middleName = 'Volodymyrovych';
+let lastName = 'Pavlovskyi';
+let person = `${firstName} ${middleName} ${lastName}`;
+
+
+let firstName2 = prompt('Enter your name');
+let middleName2 = prompt('Enter your middle name');
+let age = prompt('Enter your age');
+console.log(`${firstName2} ${middleName2} ${age}`);
+
+
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+
+
+let books = [
+        {name: 'Die Hard Java. Volume 1', pages: 3333, genre: 'horror', author: 'Stephen King'},
+        {name: 'Die Hard Java. Volume 2', pages: 3333, genre: 'horror', author: 'Howard Lovecraft'},
+        {name: 'Die Hard Java. Volume 3', pages: 3333, genre: 'horror', author: 'Alfred Hitchcock'}
+    ];
+console.log(books[0]);
+console.log(books[1]);
+console.log(books[2]);
